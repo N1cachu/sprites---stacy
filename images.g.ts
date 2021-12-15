cbaddfe42afb431afe3fdbab0c11d59c847ef8bf
N1cachu <90184060+N1cachu@users.noreply.github.com>
2021-12-15 13:07:11 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,208 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "9Fr!IaN;+fd)=HHV}gKf":
+            case "Standing still":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . 
+. . . f b b 5 5 b b f . . . 
+. . f b 5 5 5 5 5 5 b f . . 
+. f b 5 5 5 5 5 5 5 5 b f . 
+. f 5 5 5 5 4 4 5 5 5 5 f . 
+. f 5 5 5 4 4 4 4 5 5 5 f . 
+. f 5 f b f 4 4 f b f 5 f . 
+. f 5 4 1 f d d f 1 5 5 f . 
+. f 5 5 d d d d d d 5 5 f . 
+. . e f b 7 7 7 4 4 d f . . 
+. e 4 f b b 1 7 4 d d f . . 
+. 4 d f 1 1 1 f f f f . . . 
+. 4 4 f 6 6 6 6 6 6 f . . . 
+. . . f c c f f c c f . . . 
+. . . f e e . . e e f . . . 
+`;
+            case ":s@8jwM8wMQUT}lPh[[p":
+            case "Standing still - left":return img`
+. . . . f f f f f . . . . . 
+. . . f f 5 5 b b f f . . . 
+. . f 5 5 5 5 5 5 b f f . . 
+. . f 5 5 5 5 5 5 5 b f f . 
+. f 5 5 5 5 5 5 5 5 5 b f . 
+. f 5 5 4 5 5 5 5 5 5 b f . 
+. f f e 4 4 5 5 5 5 5 5 f . 
+. f f e 4 4 f b f 4 5 5 f . 
+. . f f d d f 1 4 d 4 5 f . 
+. . . f d d d d 4 f 5 5 f . 
+. . . f e 4 4 4 e e f f . . 
+. . . f 1 1 1 e d d 4 . . . 
+. . . f 1 7 7 7 d d e . . . 
+. . . f 6 6 6 7 e e f . . . 
+. . . . f c c c c f . . . . 
+. . . . . f e e e f . . . . 
+`;
+            case "{olavy=Vpd4WVbwx=KWP":
+            case "Standing still - right":return img`
+. . . . f f f f f f . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f f 5 5 5 5 5 5 5 f . . 
+. f f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 5 5 5 4 5 5 f . 
+. f 5 5 5 5 5 5 4 4 e f f . 
+. f 5 4 4 f b f 4 4 e f f . 
+. f 5 4 d 4 1 f d d f f . . 
+. f f f f 4 d d d d f . . . 
+. . . f e e 4 4 4 e f . . . 
+. . . 4 d d e 1 1 1 f . . . 
+. . . e d d 7 7 7 1 f . . . 
+. . . f e e 7 6 6 6 f . . . 
+. . . . f c c c c f . . . . 
+. . . . f e e e f . . . . . 
+`;
+            case "-R8J~TjkB~d_L{wZYp*4":
+            case "Walking - left":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f . . . . . 
+. . . f f 5 5 5 b f f . . . 
+. . f b 5 5 5 5 5 b f f . . 
+. . f 5 5 5 5 5 5 5 b f f . 
+. f 5 5 5 5 5 5 5 5 5 b f . 
+. f 5 5 4 5 5 5 5 5 5 b f . 
+. f f 5 4 4 5 5 5 5 5 b f . 
+. f f f 4 4 f 1 f 4 4 5 f . 
+. . f 5 d d f 1 4 d 4 5 f . 
+. . . f d d d d 4 5 5 5 f . 
+. . . f e 4 4 4 e d d 4 . . 
+. . . f 1 1 7 7 7 d d e . . 
+. . f c 6 6 6 6 7 e e f . . 
+. . f c c 6 f f c 6 f f . . 
+. . . f e e f . f e e . . . 
+`;
+            case "t/h:@K0S?BY_+FYvs":
+            case "Walking - right":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f f 5 5 5 5 5 5 5 f . . 
+. f f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 5 5 5 4 5 5 f . 
+. f 5 5 5 5 5 5 4 4 5 f f . 
+. f 5 4 4 f b f 4 4 f f f . 
+. f 5 4 d 4 1 f d d 5 f . . 
+. f 5 f f 4 d d d d f . . . 
+. . 4 d d e 4 4 4 e f . . . 
+. . e d d 7 7 7 1 1 f . . . 
+. . f e e 7 6 6 6 6 f f . . 
+. . f f f c c c c c f f . . 
+. . . f e . . . e e f . . . 
+`;
+            case "image1":
+            case "Standing - hurt":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 2 2 f . . 
+. . f 2 2 2 2 2 2 2 2 f . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . . f 2 2 f f 2 2 f . . . 
+. . . f 2 2 . . 2 2 f . . . 
+`;
+            case "image2":
+            case "Walking left - hurt":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f . . . . . 
+. . . f f 2 2 2 2 f f . . . 
+. . f 2 2 2 2 2 2 2 f f . . 
+. . f 2 2 2 2 2 2 2 2 f f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f f 2 2 2 2 2 2 2 2 2 f . 
+. f f f 2 2 2 2 2 2 2 2 f . 
+. . f 2 2 2 2 2 2 2 2 2 f . 
+. . . f 2 2 2 2 2 2 2 2 f . 
+. . . f 2 2 2 2 2 2 2 2 . . 
+. . . f 2 2 2 2 2 2 2 2 . . 
+. . f 2 2 2 2 2 2 2 2 f . . 
+. . f 2 2 2 f f 2 2 f f . . 
+. . . f 2 2 f . f 2 2 . . . 
+`;
+            case "image3":
+            case "Walking right - hurt":return img`
+. . . . . . . . . . . . . . 
+. . . . f f f f f f . . . . 
+. . . f f 2 2 2 2 f f . . . 
+. . f f 2 2 2 2 2 2 2 f . . 
+. f f 2 2 2 2 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 f f . 
+. f 2 2 2 2 2 2 2 2 f f f . 
+. f 2 2 2 2 2 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 f . . . 
+. . 2 2 2 2 2 2 2 2 f . . . 
+. . 2 2 2 2 2 2 2 2 f . . . 
+. . f 2 2 2 2 2 2 2 f f . . 
+. . f f 2 2 2 2 2 2 f f . . 
+. . . f 2 . . . 2 2 f . . . 
+`;
+            case "image4":
+            case "Standing still left - hurt":return img`
+. . . . f f f f f . . . . . 
+. . . f f 2 2 2 2 f f . . . 
+. . f 2 2 2 2 2 2 2 f f . . 
+. . f 2 2 2 2 2 2 2 2 f f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f f 2 2 2 2 2 2 2 2 2 f . 
+. f f 2 2 2 2 2 2 2 2 2 f . 
+. . f f 2 2 2 2 2 2 2 2 f . 
+. . . f 2 2 2 2 2 2 2 2 f . 
+. . . f 2 2 2 2 2 2 f f . . 
+. . . f 2 2 2 2 2 2 2 . . . 
+. . . f 2 2 2 2 2 2 2 . . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 f . . . . 
+`;
+            case "image5":
+            case "Standing still right - hurt":return img`
+. . . . f f f f f f . . . . 
+. . . f f 2 2 2 2 f f . . . 
+. . f f 2 2 2 2 2 2 2 f . . 
+. f f 2 2 2 2 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 f f . 
+. f 2 2 2 2 2 2 2 2 2 f f . 
+. f 2 2 2 2 2 2 2 2 f f . . 
+. f f f f 2 2 2 2 2 f . . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . . 2 2 2 2 2 2 2 f . . . 
+. . . 2 2 2 2 2 2 2 f . . . 
+. . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 f . . . . 
+. . . . f 2 2 2 f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
